79ba3043f1c6245402bbc21cace5e497ada60686
isan_mh <1157050074@student.uinsgd.ac.id.com>
2024-05-21 13:18:04 +0700

    Buttom navbar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // var untuk data
+  bool isSwitch = false;
+  bool? isCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +19,12 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
         backgroundColor: Colors.transparent,
         automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         foregroundColor: Colors.black,
         actions: [
           IconButton(
@@ -25,10 +40,97 @@ class HomePage extends StatelessWidget {
         ],
       ),
       extendBodyBehindAppBar: true,
-      body: Column(
-        children: [
-          Image.asset('assets/images/banner.png'),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            GestureDetector(
+              child: Image.asset('assets/images/banner.png'),
+              onTap: () {
+                print('banner');
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+              height: 10,
+            ),
+            Container(
+              color: isCheck == true ? Colors.green : Colors.blue,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              child: const Text(
+                'Hello World!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            // button yang ada flutter
+            ElevatedButton(
+              onPressed: () {
+                print('elevated');
+              },
+              style: ElevatedButton.styleFrom(
+                // tenary
+                backgroundColor: isSwitch ? Colors.red : Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Click Me!'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                print('OutlinedButton');
+              },
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.green,
+              ),
+              child: const Text('Click Me!'),
+            ),
+            TextButton(
+              onPressed: () {
+                print('TextButton');
+              },
+              child: const Text('Click Me!'),
+            ),
+            // gesture detector
+            GestureDetector(
+              onTap: () {
+                print('GestureDetector');
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.add),
+                  Text('Click Me!'),
+                ],
+              ),
+            ),
+            // switch
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isSwitch = newValue;
+                  print('Switch: $isSwitch');
+                });
+              },
+            ),
+            Checkbox(
+              value: isCheck,
+              onChanged: (bool? newCheck) {
+                setState(() {
+                  isCheck = newCheck;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 200,
+            ),
+            Image.network('https://reqres.in/img/faces/7-image.jpg'),
+          ],
+        ),
       ),
     );
   }
--- lib/views/root_page.dart
+++ lib/views/root_page.dart
@@ -1,29 +1,57 @@
 // import 'package:dasar_app/views/home_page.dart';
+import 'package:dasar_app/views/home_page.dart';
+import 'package:dasar_app/views/profile_page.dart';
 import 'package:flutter/material.dart';
 
-class RootPage extends StatelessWidget {
+class RootPage extends StatefulWidget {
   const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  // var untuk bottom navigation bar
+  int currentPage = 0;
+
+  // widget untuk bottom navigation bar
+  List<Widget> pages = [
+    ProfilePage(),
+    HomePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Root Page'),
+      // body: Center(
+      //   child: ElevatedButton(
+      //     onPressed: () {
+      //       Navigator.pushNamed(context, '/home');
+      //     },
+      //     child: const Text('Go to Home Page'),
+      //   ),
+      // ),
+
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.people_alt),
+            label: 'Profile',
+          ),
+        ],
+        onDestinationSelected: (int index) {
+          print('ini $index');
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/home');
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(
-            //     builder: (context) => const HomePage(),
-            //   ),
-            // );
-          },
-          child: const Text('Go to Home Page'),
-        ),
-      ),
+      body: pages[currentPage],
     );
   }
 }
